a7bc56f8d4cd641b89e74657d9626630bc387ea7
odinokun <odinokun@gmail.com>
2020-01-04 01:14:40 +0200

    edition aside menu open function

--- src/js/modules/aside.js
+++ src/js/modules/aside.js
@@ -5,16 +5,23 @@ module.exports = function () {
   // В каком положении полосы прокрутки начинать показ бокового меню
   var top_show = 300;
 
+  function asideView() {
+    if ($(this).scrollTop() > top_show) {
+      $('#aside').addClass('active');
+    }
+    else {
+      $('#aside').removeClass('active');
+    }
+  }
+
   $(document).ready(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > top_show) {
-        $('#aside').addClass('active');
-      }
-      else {
-        $('#aside').removeClass('active');
-      }
-    });
+    asideView();
   });
+
+  $(window).scroll(function () {
+    asideView();
+  });
+
   // end aside
 
 };
